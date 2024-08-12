5fb99e8d0f8fa2976d584366482a37cc1e7a454f
TaNik1 <60057477+TaNik1@users.noreply.github.com>
2024-08-12 21:42:04 +0500

    Update database.py

--- DataBase/database.py
+++ DataBase/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://TaNik:Nikita2006tarvk1@localhost/Users"
+DATABASE_URL = "mysql+pymysql://user_c0269:b$6Q5X}q/4aM@217.28.223.127:17160/db_59b9a"
 
 # Создаем соединение с базой данных
 engine = create_engine(DATABASE_URL)
