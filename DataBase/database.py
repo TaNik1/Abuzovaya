3e6fd9473d25ee7a82449419240676b928e7aba2
TaN1k <nikitat612006@gmail,com>
2024-08-12 18:19:45 +0500

    first commit

--- DataBase/database.py
+++ DataBase/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://username:password@localhost/mydatabase"
+DATABASE_URL = "mysql+pymysql://TaNik:Nikita2006tarvk1@localhost/Users"
 
 # Создаем соединение с базой данных
 engine = create_engine(DATABASE_URL)
