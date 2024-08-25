64c2d8429301c2e946524f7b19bfd3410aea42d5
TaN1k <nikitat612006@gmail,com>
2024-08-25 21:57:12 +0500

    some fix

--- Abuzovaya_bot/Bot/message.py
+++ Abuzovaya_bot/Bot/message.py
@@ -1,6 +1,6 @@
 from .bot import bot
 from .States import CreateUser
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMedia
 from aiogram.utils.exceptions import BadRequest
 from Abuzovaya_bot.utils.GridGenerator import GridGenerator
 from io import BytesIO
@@ -18,12 +18,12 @@ async def send_subscription_message(chat_id, inline_message_id=None):
         "👉🏻@ABUZOVAYA_K\n"
     )
     with open("/home/nikitat612006/d/Abuzovaya/Abuzovaya_bot/src/image/img6.jpg", 'rb') as photo:
+        photo = InputMedia(photo, caption=message_text)
         try:
-            photo = InputMediaPhoto(photo, caption=message_text)
             await bot.edit_message_media(inline_message_id=inline_message_id, media=photo,
                                          reply_markup=keyboard)
         except BadRequest:
-            message = await bot.send_photo(chat_id, photo, caption=message_text, reply_markup=keyboard)
+            message = await bot.send_photo(chat_id, photo, reply_markup=keyboard)
             await bot.delete_message(chat_id=chat_id, message_id=message.message_id - 1)
 
 
